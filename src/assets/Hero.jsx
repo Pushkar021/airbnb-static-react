387c1c3e8111d10ed8554a27373853811bf3041f
Pushkar021 <157273057+Pushkar021@users.noreply.github.com>
2024-06-02 02:04:03 +0530

    Update Hero.jsx

--- src/assets/Hero.jsx
+++ src/assets/Hero.jsx
@@ -4,7 +4,7 @@ import React from "react"
 export default function Hero() {
     return (
         <section className="hero">
-             <img src="../public/images/Hero.png" className="hero--photo"/>
+             <img src="/images/Hero.png" className="hero--photo"/>
             <h1 className="hero--header">Online Experiences</h1>
             <p className="hero--text">Join unique interactive activities led by 
             one-of-a-kind hosts—all without leaving home.</p>
